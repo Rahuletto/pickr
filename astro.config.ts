37e9e202a9952a54c2bd7d1a4d3c39ab8aaf25f3
Rahul Marban <rahulmarban@gmail.com>
2024-03-03 10:33:53 +0000

    chorus: static

--- astro.config.ts
+++ astro.config.ts
@@ -1,37 +1,33 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import compress from "astro-compress";
 import { VitePWA } from "vite-plugin-pwa";
 import { manifest, seoConfig } from "./utils/seoConfig";
 
-import vercel from '@astrojs/vercel/serverless';
- 
+import vercelStatic from "@astrojs/vercel/static";
 
 // https://astro.build/config
 export default defineConfig({
   site: seoConfig.baseURL,
-  output: 'hybrid',
-  adapter: vercel({
-    edgeMiddleware: true,
-    webAnalytics: {
-      enabled: true,
-    },
-    maxDuration: 8,
-  }),
-
+  output: "static",
+  adapter: vercelStatic(),
 
   integrations: [react(), compress()],
   vite: {
-    plugins: [VitePWA({
-      registerType: "autoUpdate",
-      manifest,
-      workbox: {
-        globDirectory: 'dist',
-        globPatterns: ['**/*.{js,css,svg,png,jpg,jpeg,gif,webp,woff,woff2,ttf,eot,ico}'],
-        // Don't fallback on document based (e.g. `/some-page`) requests
-        // This removes an errant console.log message from showing up.
-        navigateFallback: null
-      }
-    })]
-  }
+    plugins: [
+      VitePWA({
+        registerType: "autoUpdate",
+        manifest,
+        workbox: {
+          globDirectory: "dist",
+          globPatterns: [
+            "**/*.{js,css,svg,png,jpg,jpeg,gif,webp,woff,woff2,ttf,eot,ico}",
+          ],
+          // Don't fallback on document based (e.g. `/some-page`) requests
+          // This removes an errant console.log message from showing up.
+          navigateFallback: null,
+        },
+      }),
+    ],
+  },
 });
