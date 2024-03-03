680b37515d1994c4f26872b0c50aaeb1fe244904
Rahul Marban <rahulmarban@gmail.com>
2024-03-03 10:32:04 +0000

    chorus: vercel it

--- astro.config.ts
+++ astro.config.ts
@@ -1,42 +1,37 @@
 import { defineConfig } from 'astro/config';
-
 import react from "@astrojs/react";
-import compress from "astro-compress"
-import { VitePWA } from "vite-plugin-pwa"
-import cloudflare from "@astrojs/cloudflare";
+import compress from "astro-compress";
+import { VitePWA } from "vite-plugin-pwa";
+import { manifest, seoConfig } from "./utils/seoConfig";
 
-import { manifest, seoConfig } from "./utils/seoConfig"
+import vercel from '@astrojs/vercel/serverless';
+ 
 
+// https://astro.build/config
 export default defineConfig({
-  output: "hybrid",
-  adapter: cloudflare({
-    imageService: "cloudflare",
-    runtime: {
-      mode: "local",
-      type: "pages",
+  site: seoConfig.baseURL,
+  output: 'hybrid',
+  adapter: vercel({
+    edgeMiddleware: true,
+    webAnalytics: {
+      enabled: true,
     },
+    maxDuration: 8,
   }),
 
-	site: seoConfig.baseURL,
-	integrations: [
-		react(),
-		compress()
-	],
-	vite: {
-		plugins: [
-			VitePWA({
-				registerType: "autoUpdate",
-				manifest,
-				workbox: {
-				  globDirectory: 'dist',
-				  globPatterns: [
-				    '**/*.{js,css,svg,png,jpg,jpeg,gif,webp,woff,woff2,ttf,eot,ico}',
-				  ],
-				  // Don't fallback on document based (e.g. `/some-page`) requests
-				  // This removes an errant console.log message from showing up.
-				  navigateFallback: null,
-				},
-			})
-		]
-	}
-})
+
+  integrations: [react(), compress()],
+  vite: {
+    plugins: [VitePWA({
+      registerType: "autoUpdate",
+      manifest,
+      workbox: {
+        globDirectory: 'dist',
+        globPatterns: ['**/*.{js,css,svg,png,jpg,jpeg,gif,webp,woff,woff2,ttf,eot,ico}'],
+        // Don't fallback on document based (e.g. `/some-page`) requests
+        // This removes an errant console.log message from showing up.
+        navigateFallback: null
+      }
+    })]
+  }
+});
