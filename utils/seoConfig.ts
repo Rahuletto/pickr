b4f136c44ecaf14e168352ea8c318171f12924b3
Rahul Marban <rahulmarban@gmail.com>
2024-03-03 10:06:51 +0000

    pwa

--- utils/seoConfig.ts
+++ utils/seoConfig.ts
@@ -1,50 +1,24 @@
-
-import type { ManifestOptions } from "vite-plugin-pwa"
+import type { ManifestOptions } from "vite-plugin-pwa";
 
 export const seoConfig = {
-	baseURL: "https://example.com",
-	description:
-		"Astro PWA Starter is an opionated Astro starter for building robust static websites.", 
-	type: "website",
-	image: {
-		url: "https://picsum.photos/1200/630", 
-		alt: "OpenGraph thumbnail description.",
-		width: 1200,
-		height: 630
-	},
-	siteName: "Astro PWA Starter", // Change this to your website's name,
-	twitter: {
-		card: "summary_large_image"
-	}
-}
+  baseURL: "https://pickr.pages.dev",
+  description: "Saw a color and loved it? Just Pick it and get its hex code!",
+  type: "website",
+  siteName: "Pickr",
+};
 
-/**
- * Defines the configuration for PWA webmanifest.
- */
 export const manifest: Partial<ManifestOptions> = {
-	name: "Astro PWA Starter", // Change this to your website's name.
-	short_name: "Astro PWA Starter", // Change this to your website's short name.
-	description:
-		"Astro PWA Starter is an opionated Astro starter for building robust static websites.", // Change this to your websites description.
-	theme_color: "#30E130", // Change this to your primary color.
-	background_color: "#ffffff", // Change this to your background color.
-	display: "minimal-ui",
-	icons: [
-		{
-			src: "/favicons/favicon-192x192.png",
-			sizes: "192x192",
-			type: "image/png"
-		},
-		{
-			src: "/favicons/favicon-512x512.png",
-			sizes: "512x512",
-			type: "image/png"
-		},
-		{
-			src: "/favicons/favicon-512x512.png",
-			sizes: "512x512",
-			type: "image/png",
-			purpose: "any maskable"
-		}
-	]
-}
+  name: "Pickr",
+  short_name: "Pickr",
+  description: "Saw a color and loved it? Just Pick it and get its hex code!",
+  theme_color: "#070707",
+  background_color: "#070707",
+  display: "minimal-ui",
+  icons: [
+    {
+      src: "/public/favicon.svg",
+      sizes: "192x192",
+      type: "image/svg",
+    },
+  ],
+};
